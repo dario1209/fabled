3937989e308b4a1deaef7a74fa25eadd88640cd6
dario1209 <77805985+dario1209@users.noreply.github.com>
2024-06-06 05:01:31 -0700

    Update BidButton.tsx

--- src/components/AuctionItem/BidButton.tsx
+++ src/components/AuctionItem/BidButton.tsx
@@ -93,7 +93,7 @@ const BidButton = ({
           setNeedApprove={setNeedApprove}
         />
       ) : isBidConfirming || allowancePending ? (
-        <button disabled className="bid-button">
+        <button enabled className="bid-button">
           Loading...
         </button>
       ) : (
